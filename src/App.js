bc5404e667f27c41e358e3c1942169e30d1631e5
Carlo Bruno <carlofbruno@gmail.com>
2018-09-22 19:22:45 -0700

    initialized google maps
    added Google Maps API

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.renderMap();
+  }
+
+  renderMap = () => {
+    loadScript(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCKp1_4GchKu7yMf2H6CmMo04Fs2SWocYA&callback=initMap"
+    );
+    window.initMap = this.initMap;
+  };
+
+  initMap = () => {
+    const map = new window.google.maps.Map(
+      document.getElementById("map"),
+      {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8
+      }
+    );
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to
-          reload.
-        </p>
-      </div>
+      <main>
+        <div id="map" />
+      </main>
     );
   }
+}
+
+/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
+  async defer></script> */
+
+// AIzaSyCKp1_4GchKu7yMf2H6CmMo04Fs2SWocYA
+
+function loadScript(url) {
+  const index = window.document.getElementsByTagName("script")[0];
+
+  const script = window.document.createElement("script");
+
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
